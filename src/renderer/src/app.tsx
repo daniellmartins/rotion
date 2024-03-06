1169b530647b8fc088dac5e4b1396461544803a4
Daniel Martins <daniellmarttins@hotmail.com>
2024-03-05 23:04:39 -0300

    feat: add colors

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -1,6 +1,6 @@
 export function App() {
   return (
-    <div>
+    <div className="w-screen h-screen bg-rotion-900 text-rotion-100">
       <h1 className="text-5xl">Hello, world!</h1>
     </div>
   )
