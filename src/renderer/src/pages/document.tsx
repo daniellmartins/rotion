b87cc9daa405b5ff53bee8e403ea6cae5d8e0d33
Daniel Martins <daniellmarttins@hotmail.com>
2024-03-06 08:57:52 -0300

    feat: add aside document page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,8 +1,19 @@
 import { Link } from 'react-router-dom'
 
+import { ToC } from '../components/toc'
+
 export function DocumentPage() {
   return (
-    <div className="flex flex-1 justify-center items-center text-rotion-400">
+    <div className="flex flex-1 py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold text-xs uppercase">
+          Table of content
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+        </ToC.Root>
+      </aside>
       <h1>Documento</h1>
 
       <Link to="/">Voltar</Link>
